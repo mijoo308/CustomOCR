8142592f980bfa3a22332eb431cee9eca6625e77
mijoo308 <mijoo308@gmail.com>
2021-11-05 17:44:33 +0900

    add : bounding box margin

--- utils.py
+++ utils.py
@@ -14,14 +14,36 @@ def get_row_list(lt_rb_list, threshold=3):
 
     return sorted_row_groups
 
+def fix_format(sorted_row_list, margin=0):
+    result_list = []
+    for row in sorted_row_list:
+        for box in row: ##   l t r b
+            l = box[0][0]-margin
+            t = box[0][1]-margin
+            r = box[1][0]+margin
+            b = box[1][1]+margin
 
-def get_merged_row_list(sorted_row_list):
+            if l<0: l = 0
+            if t<0 : t = 0
+            result_list.append([(l, t), (r, b)])
+    
+    return result_list
+        
+
+
+    return 
+def get_merged_row_list(sorted_row_list, margin=0):
     merged_row_list = []
     for row in sorted_row_list:
-        left = min(row, key = lambda x : x[0][0])[0][0]
-        top = min(row, key = lambda x : x[0][1])[0][1]
-        right = max(row, key = lambda x : x[1][0])[1][0]
-        bottom = max(row, key = lambda x : x[1][1])[1][1]
+        left = min(row, key = lambda x : x[0][0])[0][0] - margin
+        top = min(row, key = lambda x : x[0][1])[0][1] - margin
+        right = max(row, key = lambda x : x[1][0])[1][0] + margin
+        bottom = max(row, key = lambda x : x[1][1])[1][1] + margin
+
+        if left<0: left = 0
+        if top<0: top = 0
+
+
         merged_row_list.append([(left, top),(right, bottom)])
     
     return merged_row_list
