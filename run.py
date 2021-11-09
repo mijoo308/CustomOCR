93cdca152934d739b6fab5dbf8f48e2432ee6575
mijoo308 <mijoo308@gmail.com>
2021-11-09 18:13:12 +0900

    refactor : option setting

--- run.py
+++ run.py
@@ -7,32 +7,33 @@ import utils
 import shutil
 
 def main():
-    test_img_path = r'C:\Users\beyon\Desktop\CustomOCR\news_test\news.jpg'
-    threshold = 0.5
+    
+    ''' option'''
+    test_img_path = r'C:\Users\beyon\Desktop\CustomOCR\new_news\news3.jpg'
+    detection_threshold = 0.5
     detection_model = r'./craft_onnx/detector_craft.onnx'
-    img_name = os.path.basename(test_img_path).split('.')[0]
     im = cv2.imread(test_img_path)
-
     recog_model = ''
-    transformation = 'TPS'
     h,w,_ = im.shape
+    
+    TRANSFORMATION = 'None'
+    FEATURE_EXTRACTION = 'ResNet'
+    SEQUENCE = 'BiLSTM'
+    PREDICTION = 'CTC'
 
-    feature_extraction = 'ResNet'
-    sequence = 'BiLSTM'
-    prediction = 'Attn'
+    img_name = os.path.basename(test_img_path).split('.')[0]
 
-    opt = OPT(recog_model, transformation, feature_extraction, sequence, prediction)
+    opt = OPT(recog_model, TRANSFORMATION, FEATURE_EXTRACTION, SEQUENCE, PREDICTION)
     model, converter = load_model(opt)
     
     '''detection'''
-    boxes = detect(detection_model, im, threshold)
+    boxes = detect(detection_model, im, detection_threshold)
 
     ltrb_list = []  ## [[(tl),(br)],[],[], .... ]
     for box in boxes:
         tl = tuple(box[0])
         br = tuple(box[2])
         ltrb_list.append([tl, br])
-        # im = cv2.rectangle(im, tl, br, (255, 0, 0), 2)
 
     ltrb_list = sorted(ltrb_list, key = lambda x : x[0][1])
 
@@ -41,7 +42,6 @@ def main():
     # merged_row_list = utils.get_merged_row_list(sorted_row_list, h, w)
 
 
-    ### TODO: Test 필요
 
     temp_detection_dir = tempfile.mkdtemp(prefix=img_name)
 
@@ -50,6 +50,7 @@ def main():
     '''recognition'''
     cnt = 0
     for box in fixed_row_list:
+    for box in merged_row_list:
         # print(box[0][0], box[0][1], box[1][0], box[1][1])
         l, t, r, b = box[0][0], box[0][1], box[1][0], box[1][1]
         # l, t, r, b = box[0][0][0], box[0][0][1], box[0][1][0], box[0][1][1]
